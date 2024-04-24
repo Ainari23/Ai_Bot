6ce097d4cbe67cb8de20bfdc5ebc334ca8936402
Votre Nom <iantsa.nyaina23@gmail.com>
2024-04-24 11:05:14 +0300

    simple commmit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chatapp/chat_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChatScreen(),
+    );
+  }
+}
